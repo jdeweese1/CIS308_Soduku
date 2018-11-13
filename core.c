5674333663fb4cc0a3883ef387971fdc683d645f
Cody Murrell <cmurrell@cougar.cs.ksu.edu>
2018-11-13 14:28:34 -0600

    Added helper methods for solving

--- core.c
+++ core.c
@@ -5,3 +5,39 @@
 
 
 
+int find_assigned(board * suduko, int * row, int * col)
+{
+	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < 9; j++) {
+			if(suduko->suduko_board[i][j] == 0) {
+				row = i;
+				col = j;
+				return TRUE;
+			}
+		}
+	}
+	row = 0;
+	col = 0;
+	return FALSE;
+}
+
+int used_in_row(board * suduko, int * row, int num) 
+{
+	for(int col = 0; col < 9; col++) {
+		if(suduko-sudoku_board[*row][col] == num) {
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+int used_in_col(board * suduko, int * col, int num)
+{
+        for(int row = 0; row < 9; row++) {
+                if(suduko-sudoku_board[row][*col] == num) {
+                        return TRUE;
+                }
+        }
+        return FALSE;
+}
+
