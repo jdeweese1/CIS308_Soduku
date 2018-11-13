0930a75573c099682a1fe4132b15aa9910baf0d3
Jarod DeWeese <jaroddeweese@gmail.com>
2018-11-13 15:16:11 -0600

    Added more skeleton

--- core.c
+++ core.c
@@ -3,5 +3,19 @@
 
 #include "core.h"
 
-
-
+int check_board(board *)
+{
+	return FALSE;
+}
+board * solve_board(board *)
+{
+	return NULL;
+}
+board * generate_board()
+{
+	return NULL;
+}
+int core_main(int argc, const char * argv[]) 
+{
+	return 0;
+}
