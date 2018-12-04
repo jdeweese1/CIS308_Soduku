d0f735eef1b93e5c3b14321c504a784cd08c1bff
Jarod DeWeese <32393906+jdeweese1@users.noreply.github.com>
2018-12-03 23:41:01 -0600

    Jarod (#23)
    * Modified autorunner and fixed merge problems
    
    * added to autorunner
    
    * changed autorunner and added numbers for tests
    
    * foobar
    
    * final tweaks
    
    * added lots of data
    
    * made directory in bash script
    
    * made clean
    
    * made generate_board work

--- autorunner.sh
+++ autorunner.sh
@@ -1,7 +1,10 @@
-
-echo "$(tput setaf 6)rm'ing out.txt\n"
+#CYAN 6
+#YELLOW 3
+#MAGENTA 5
+echo "$(tput setaf 6)rm'ing out.txt"
 rm out.txt
-echo "$(tput setaf 6)cleaning files\n"
+echo "$(tput setaf 6)Cleaning files..."
+
 make clean > /dev/null
 
 
@@ -11,44 +14,61 @@ make main > /dev/null
 # We can solve
 echo "$(tput sgr 0)"
 
-echo "About to solve a board by running './main solve -i unsolved1.txt -o out.txt'"
+
+echo "About to solve a board by running $(tput setaf 3)'./main solve -i unsolved1.txt -o out.txt' $(tput sgr 0)"
 echo "Here is the initial value of the board passed, press enter to continue"
-echo "$(tput sgr 0)"
+echo "$(tput setaf 5)"
+
 cat unsolved1.txt
+echo "$(tput sgr 0)"
 
 read LINE
 
 ./main solve -i unsolved1.txt -o out.txt > /dev/null
-echo "About to cat out.txt, press enter to continue"
+echo "About to $(tput setaf 3)cat out.txt $(tput sgr 0), press enter to continue"
 read LINE
 echo "$(tput sgr 0)"
 
+echo "$(tput setaf 5)"
+cat out.txt
+echo "$(tput sgr 0)"
+
+echo ""
+
+# We can generate
+tput setaf 6
+echo "rm'ing out.txt"
+rm out.txt
+tput sgr 0
+echo "About to generate a board by running $(tput setaf 3)'./main generate -o out.txt'"
+tput sgr 0
+read LINE
+./main generate -o out.txt
+tput sgr 0
+echo "About to $(tput setaf 3)cat out.txt $(tput sgr 0), press enter to continue"
+read LINE
+tput setaf 5
 cat out.txt
 echo "\n\n"
-
-# # We can generate
-# echo "rm'ing out.txt"
-# rm out.txt
-# echo "About to generate a board by running './main generate -o out.txt'"
-# read LINE
-# ./main generate -o out.txt
-# echo "About to cat out.txt, press enter to continue"
-# read LINE
-# cat out.txt
-# echo "\n\n"
+tput sgr 0
 
 # We can check that a board is valid
-echo "About to validate a board is solved by running './main check -i solved1.txt'"
+echo "About to validate a board is solved by running $(tput setaf 3)'./main check -i solved1.txt'"
 read LINE
+tput setaf 5
 ./main check -i solved1.txt
+tput sgr 0
 echo "\n\n"
 
 # Check board is invalid
-echo "$(tput setaf 2)About to check a board bad './main check -i bad.txt'"
+echo "About to check a board bad $(tput setaf 3)'./main check -i bad.txt'$(tput sgr 0)"
 read LINE
+tput setaf 5
 ./main check -i bad.txt
+tput sgr 0
 
-# Make tests
+make test
+
 make test > /dev/null
 echo "\n"
 echo "About to run entire test suite"
@@ -56,29 +76,32 @@ read LINE
 # We can also run tests
 ./test
 
-echo "\n\n"
-echo "About to run only board tests"
-read LINE
-./test -board
+# echo "\n\n"
+# echo "About to run only board tests"
+# read LINE
+# ./test -board
 
-echo "About to run only dictionary tests"
-read LINE
-./test -dict
+# echo "About to run only dictionary tests"
+# read LINE
+# ./test -dict
 
 
 echo "About to solve 100 boards"
-
 mkdir ./datafiles/solved > /dev/null
+rm ./datafiles/solved/*
 
-make main
+make main > /dev/null
 init_time="$(date +%s)"
 count=0
-for file in ls -d "$PWD"/datafiles/unsolved/*
+for file in "$PWD"/datafiles/unsolved/*
+
 do
     ((count+=1))
     temp="$count solved.txt"
     echo "$temp"
+    tput setaf 5
     ./main solve -i $file -o "$PWD"/datafiles/solved/"$temp"
+    tput sgr 0
 done 
 final_time="$(date +%s)"
 echo $((final_time-init_time)) "seconds to solve $count boards"
--- core.h
+++ core.h
@@ -49,7 +49,7 @@ int is_safe(Board * sudoku, int * col, int * row, int num);
 
 int check_board(Board * b);
 int solve_board(Board * b); 
-Board * generate_board();
+int populate_board(Board * b);
 void print_board(Board * b);
 
 // Methods for a SimpleDict
